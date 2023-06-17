87eb73a368140bb19c83e62942d732b22e3f0138
Adrian3009 <adrian.suruianu@alumnos.upm.es>
2023-06-17 13:52:55 +0200

    Create pruebas_del_trabajo.cpp

---
+++ pruebas_del_trabajo.cpp
@@ -0,0 +1,65 @@
+/**/
+
+
+#include <stdio.h>
+
+
+typedef struct
+{
+	char linea[400];
+}lineas;
+
+typedef struct
+{
+	char tipo[300];
+}tipos;
+
+typedef struct
+{
+	float num[];
+}datos;
+
+int main()
+{
+	lineas c[22];
+	tipos t[22];
+	datos n[24];
+	int i = 0, j = 0;
+	FILE *pf;
+	pf = fopen("C:/Users/Adrian/OneDrive/Documentos/Informatica/generacion_por_tecnologias_21_22_puntos_simplificado.csv","r");
+	if (pf == NULL)
+    {
+        printf("Error al abrir el fichero.\n");
+        return -1;
+    }
+	
+	int contador = 0;
+
+    while (fgets(c[i].linea, sizeof(c[i].linea), pf) != NULL) 
+	{
+        contador++;
+
+        if (contador >= 5) 
+		{
+            //printf("%s\n", c[i].linea);
+            fscanf(pf,"%[^,]",t[i].tipo);
+            i++;
+        }
+    }
+    
+    for(i=0;i<17;i++)
+    {
+    	printf("%s\n", t[i].tipo);
+	}
+
+    fclose(pf);
+	
+    
+    
+    
+
+	
+	return 0;
+}
+
+
