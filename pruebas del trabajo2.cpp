d9f4ce56d110234f1a79362b67110afb062e91fc
Lucas2758 <130782362+Lucas2758@users.noreply.github.com>
2023-06-19 15:05:12 +0200

    cambiar datos a numeros reales
    Los datos estan en la memoria correcta mente como numeros reales
    He empezado un menu que todavia no funciona bien hay que acabarlo y mirarlo bien

--- pruebas del trabajo2.cpp
+++ pruebas del trabajo2.cpp
@@ -12,13 +12,13 @@ typedef struct
 typedef struct
 {
 	char tipo[35];
-	float dato[24];
+	double dato[24];
 }datos;
 
 struct extra
 {
-	char cardato[30];
-}extradatos[24];
+	char cardato[50];
+}extradatos[408];
 
 //void DATOS(char *,float *);
 
@@ -30,6 +30,7 @@ int main()
 	lin *numlineas; //El puntero de estructuras que cambiaremos a vector con dimension n.
 	FILE *fich;
 	int n=0,i=0,j=0,k=0,numcomas=0,q=0,d=0,l=0,m=0;
+	char c;
 	datos numdatos[17];
 	if((fich=fopen("generacion_por_tecnologias_21_22_puntos_simplificado.csv","r"))==NULL)printf("Problema al abrir fichero"); //Apertura
 	
@@ -37,7 +38,7 @@ int main()
 	{
 		if(fgetc(fich)=='\n') n++;
 	}
-	printf("El numero total de lineas en el archivo es %i",n);
+	//printf("El numero total de lineas en el archivo es %i",n);
 	//Nos da el valor n igual a 22.
 	numlineas=(lin*)malloc(n*sizeof(lin));//El puntero lo hemos cambiado a un vector con dimension "n".
 	rewind(fich);
@@ -63,11 +64,14 @@ int main()
 		printf("\n%s",numlineas[i].lineas);
 	}*/
 	
+	
+	
 	/*Lo siguente que tenemos que hacer es ordenar los nombres de los tipos en la segunda estructura 
 	y cambiar los numeros de caracteres a floats(numeros reales)*/
 	
-	//Para ello encontramos la coma en cada cadena
 	
+	//Para ello encontramos la coma en cada cadena
+	//Al mismo tiempo lo copiamos
 	for(i=5,k=0;i<n;i++,k++)
 	{
 		for(j=0,numcomas=0;numcomas<1;j++)
@@ -75,18 +79,20 @@ int main()
 			if(numlineas[i].lineas[j]==',')
 			{
 				numcomas++;
-				printf("\nEn la linea %i la primera coma se encuentra en la posicion %i",i,j);
+				//printf("\nEn la linea %i la primera coma se encuentra en la posicion %i",i,j);
 				//Ahora copiamos en la segunda estructura el nombre del tipo correspondiente
 				memcpy(numdatos[k].tipo,numlineas[i].lineas,j*sizeof(char));
 			}
 		}
 	}
 	
-	for(i=0;i<17;i++)
+	//Ver si los nombres de los tipos estan bien copiados 
+	/*for(i=0;i<17;i++)
 	{
 		printf("\n%s",numdatos[i].tipo);
-	}
+	}*/
 	
+	//Ahora copiamos los datos de cada tipo en CARACTERES en otras cadenas.
 	for(i=5,k=0;i<n;i++,k++)
 	{
 		for(j=0,numcomas=0;numcomas<1;j++)
@@ -94,32 +100,60 @@ int main()
 			if(numlineas[i].lineas[j]==',')
 			{
 				numcomas++;
-				printf("\nEn la linea %i la primera coma se encuentra en la posicion %i",i,j);
+				//printf("\nEn la linea %i la primera coma se encuentra en la posicion %i",i,j);
 				q=j;
 				for(m=0;m<24;m++)
 				{
 					q++;
-					printf("Valor de q= %i",q);
-					printf("\t%c\t",numlineas[5].lineas[11]);
+					//printf("Valor de q= %i",q);
+					//printf("\t%c\t",numlineas[5].lineas[11]);
 					d=0;
-					while(numlineas[i].lineas[q]!=',')
+					while(numlineas[i].lineas[q]!=',')//***Aqui es donde lo copiamos***//
 					{
-						extradatos[l].cardato[d]=numlineas[i].lineas[q];
-						d++;
-						q++;
-						printf("\t%s\t",extradatos[l].cardato);
+						extradatos[l].cardato[d]=numlineas[i].lineas[q];/*Se copia cada numero que esta entre comas
+																		desde el primer coma hasta el último*/
+						d++;											//La d sirve para la posicion en la cadena en la que se copia el numero
+						q++;											//La posicion de la coma está indicada por la q esta sigue la cadena copiada
+						//printf("\t%s\t",extradatos[l].cardato);
 					}
-					l++;
-						
+					l++;		
 				}
 			}
 		}
 	}
 	
-	//Ahora tenemos que copiar los datos y cambiarlos a numeros real
-	
-	printf("%s",extradatos[0].cardato);
-	
+	k=0;
+	for(i=0;i<17;i++)
+	{
+		for(j=0;j<24;j++)
+		{
+			numdatos[i].dato[j]=atof(extradatos[k].cardato);
+			k++;
+		}
+	}
+	while(c='s')
+	{
+		printf("Quiere entrar en menu de encontrar datos? escriba s o n\n");
+		scanf("%c",&c);
+		if(c=='s')
+		{
+			printf("Menu de encontrar datos que dato desea\nTipo de generacion(en numero):");
+			scanf("%i",&i);
+			printf("Numero de dato:");
+			scanf("%i",&j);
+			printf("El dato pedido es %.15f\n",numdatos[i-1].dato[j-1]);
+		}	
+	}
+
+	for(i=0;i<17;i++)
+	{
+		for(j=0;j<24;j++)
+		{
+			printf("El dato %i del tipo  en numeros reales es %f",numdatos[i].dato[j]);
+    	}
+	}
+
+	//Ahora tenemos que cambiar los datos a numeros real
 	
 		
 	/*for(i=5;i<n;i++)
