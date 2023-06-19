6e9159a1b56336f880083f556ab2e4afaa77a1de
Lucas2758 <130782362+Lucas2758@users.noreply.github.com>
2023-06-19 10:34:33 +0200

    cosas copiadas en cadenas

--- pruebas del trabajo2.cpp
+++ pruebas del trabajo2.cpp
@@ -11,7 +11,7 @@ typedef struct
 
 typedef struct
 {
-	char tipo[];
+	char tipo[60];
 	float dato[24];
 }datos;
 
@@ -21,7 +21,7 @@ int main()
 	//La cantidad de lineas "n" sera la dimension de nuestro vector de estructuras "lin" cada elemento de este vector tendra una linea.
 	lin *numlineas; //El puntero de estructuras que cambiaremos a vector con dimension n.
 	FILE *fich;
-	int n;
+	int n,i;
 	if((fich=fopen("generacion_por_tecnologias_21_22_puntos_simplificado.csv","r"))==NULL)printf("Problema al abrir fichero"); //Apertura
 	
 	while(!feof(fich))
@@ -31,7 +31,20 @@ int main()
 	printf("El numero total de lineas en el archivo es %i",n);
 	//Nos da el valor n igual a 22.
 	numlineas=(lin*)malloc(n*sizeof(lin));//El puntero lo hemos cambiado a un vector con dimension "n".
-	
+	rewind(fich);
+	//Ahora copiamos en cada elemento de numlineas una cadena(linea).
+	for(i=0;i<n;i++)
+	{
+		fgets(numlineas[i].lineas,sizeof(numlineas[i].lineas),fich);//Esto copia cada fila en cada elemento
+		//Lo que pasa es que el fgets tambien copia el \n asi que hay que cambiarlo por un , para cerrar la linea
+		numlineas[i].lineas[strlen(numlineas[i].lineas)-1]=',';
+		//Hay que cerrar la cadena obviamente se añade un '\0'
+		numlineas[i].lineas[strlen(numlineas[i].lineas)]='\0';
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%s",numlineas[i].lineas);
+	}
 	
 	
 	
