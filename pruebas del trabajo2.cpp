f8ac79b41fb1afb821e4f88cc690d6f37fa8254c
Lucas2758 <130782362+Lucas2758@users.noreply.github.com>
2023-06-19 13:00:22 +0200

    Copiar datos
    Estoy copiando los datos para poder cambiarlos a floats

--- pruebas del trabajo2.cpp
+++ pruebas del trabajo2.cpp
@@ -15,13 +15,21 @@ typedef struct
 	float dato[24];
 }datos;
 
+struct extra
+{
+	char cardato[30];
+}extradatos[24];
+
+//void DATOS(char *,float *);
+
+
 int main()
 {
 	//Primero hay que contar la cantidad de lineas que tenemos.
 	//La cantidad de lineas "n" sera la dimension de nuestro vector de estructuras "lin" cada elemento de este vector tendra una linea.
 	lin *numlineas; //El puntero de estructuras que cambiaremos a vector con dimension n.
 	FILE *fich;
-	int n,i,j,k,numcomas;
+	int n=0,i=0,j=0,k=0,numcomas=0,q=0,d=0,l=0,m=0;
 	datos numdatos[17];
 	if((fich=fopen("generacion_por_tecnologias_21_22_puntos_simplificado.csv","r"))==NULL)printf("Problema al abrir fichero"); //Apertura
 	
@@ -59,6 +67,7 @@ int main()
 	y cambiar los numeros de caracteres a floats(numeros reales)*/
 	
 	//Para ello encontramos la coma en cada cadena
+	
 	for(i=5,k=0;i<n;i++,k++)
 	{
 		for(j=0,numcomas=0;numcomas<1;j++)
@@ -68,21 +77,62 @@ int main()
 				numcomas++;
 				printf("\nEn la linea %i la primera coma se encuentra en la posicion %i",i,j);
 				//Ahora copiamos en la segunda estructura el nombre del tipo correspondiente
-				memcpy(numdatos[k].tipo,numlineas[i].lineas,j*sizeof(char));	
+				memcpy(numdatos[k].tipo,numlineas[i].lineas,j*sizeof(char));
 			}
 		}
 	}
+	
 	for(i=0;i<17;i++)
 	{
 		printf("\n%s",numdatos[i].tipo);
 	}
 	
+	for(i=5,k=0;i<n;i++,k++)
+	{
+		for(j=0,numcomas=0;numcomas<1;j++)
+		{
+			if(numlineas[i].lineas[j]==',')
+			{
+				numcomas++;
+				printf("\nEn la linea %i la primera coma se encuentra en la posicion %i",i,j);
+				q=j;
+				for(m=0;m<24;m++)
+				{
+					q++;
+					printf("Valor de q= %i",q);
+					printf("\t%c\t",numlineas[5].lineas[11]);
+					while(numlineas[i].lineas[q]!=',')
+					{
+						extradatos[l].cardato[d]=numlineas[i].lineas[q];
+						d++;
+						l++;
+						q++;
+					}
+						
+				}
+			}
+		}
+	}
+	
+	//Ahora tenemos que copiar los datos y cambiarlos a numeros real
+	
+	printf("%s",extradatos[0].cardato);
 	
 	
-	
-	
-	
+		
+	/*for(i=5;i<n;i++)
+	{
+		DATOS(numlineas[i].lineas,numdatos[i].dato);
+	}
+}
+void DATOS(char *numlineas,float *numdatos)
+{
+	for()*/
 	
 	
 	
 }
+
+
+
+
