15182bdad5795f813c950e777adad07cb7047828
Lucas2758 <130782362+Lucas2758@users.noreply.github.com>
2023-06-21 11:39:41 +0200

    Calculo de media
    La función que calcula la media esta hecha. Separamos los dos años

--- código/pruebas del trabajo2.cpp
+++ código/pruebas del trabajo2.cpp
@@ -22,10 +22,15 @@ struct extra
 
 void RECOGER(FILE *);
 int BUSCARDATO(int,int);
+float media(int);
+//float mediana(int);
+//float desviacion(int);
+//float max(int);
+//float min(int);
 
 int main()
 {
-	int tecla,i=0,j=0,x=0;
+	int tecla,i=0,j=0,x=0,e=0;
 	FILE *fich;
 	if((fich=fopen("generacion_por_tecnologias_21_22_puntos_simplificado.csv","r"))==NULL)printf("Problema al abrir fichero"); //Apertura
 	
@@ -53,7 +58,48 @@ int main()
 			BUSCARDATO(x,j);		
     		break;
     	case 2:
-    		printf("");
+    		printf("Que calculo desea?\n");
+    		for(i=0;i<4;i++)
+    		{
+    			printf("%i)",i+1);
+    			if(i==0)
+				{
+    				printf("Calculo de media anual.\n");	
+				}
+				else if(i==1)
+				{
+					printf("Calculo de mediana anual.\n");
+				}
+				else if(i==2)
+				{
+					printf("Calculo de desviacion tipica anual.\n");
+				}
+				else if(i==3)
+				{
+					printf("Calculo del maximo y minimo anual.\n");
+				}
+			}
+			scanf("%i",&x);
+			if(x==1)
+			{
+				printf("De que año desea calcular medias? 1 para el primero(2021) 2 para el segundo(2022):\n");
+				scanf("%i",&e);
+				while(e==1)
+				{
+					printf("La media anual deseada es %f",media(1));
+					printf("\nDesea calcular otra media de 2021? 1 para si, cualquier otro numero para no:\n");
+					scanf("%i",&e);
+				}
+				while(e==2)
+				{
+					printf("La media anual deseada es %f",media(2));
+					printf("\nDesea calcular otra media de 2022? 2 para si, cualquier otro numero para no:\n");
+					scanf("%i",&e);
+				}
+			}
+			//else if(x==2)printf("La mediana anual deseada es %f",mediana(x));
+			//else if(x==3)printf("La desviacion tipica anual deseada es %f",desviacion(x));
+			//else if(x==4)printf("El maximo anual es %f y el minimo es %f",max(x),min(x));
     		break;
 	}
 }
@@ -170,6 +216,37 @@ int BUSCARDATO(int tipo, int numdato)
 		return 1;
 }
 
+float media(int r)
+{
+	int i=0,x=0;
+	float media=0;
+	printf("De que tipo de generacion quiere calcular la media?:\n");
+	for(i=0;i<17;i++)
+    {
+    	printf("%i) %s\n", i+1 ,numdatos[i].tipo);
+	}
+	scanf("%i",&x);
+	if(r==1)
+	{
+			for(i=0;i<12;i++)
+			{
+				media+=numdatos[x-1].dato[i];
+				printf("%f\t",media);
+			}	
+	}
+	else if(r==2)
+	{
+		for(i=12;i<24;i++)
+			{
+				media+=numdatos[x-1].dato[i];
+				printf("%f\t",media);
+			}
+	}
+	media/=12;
+	return media;
+	
+	
+}
 
 
 
